f0ac5904ef3a35d7545773b4783f31f17ff54fee
Víctor Cárdenas <victor.cardenas@estudiantat.upc.edu>
2025-09-26 09:57:49 +0200

    Week2 homework

--- GameJamEngine.cpp
+++ GameJamEngine.cpp
@@ -1,13 +1,247 @@
 ﻿#include <iostream>
 
+// OpenGL
+#include <glad/glad.h>
+#include <SDL3/SDL.h>
+
+// GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// ImGui
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_opengl3.h"
+
+// Setup VS and PS in GLSL
+const char* vertexShaderSource = "\n"
+"#version 460 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "\n"
+"#version 460 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\0";
+
 int main()
 {
-	glm::mat4 rotationMatrix(1.0f);
-	
-	std::cout << "Hello CMake and vcpkg." << std::endl;
-	return 0;
+    // Window Resolution
+    const int SCREEN_WIDTH = 1920;
+    const int SCREEN_HEIGHT = 1080;
+
+    // Init SDL
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        return -1;
+    }
+
+    // Setup Min/Major version for using OpenGL 4.6
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+    // Set Core Profile Mode
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // Create OpenGL window using SDL
+    SDL_Window* window = SDL_CreateWindow("Prueba",
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+
+    // Early out if window not valid
+    if (window == nullptr)
+    {
+        SDL_Quit();
+        return -1;
+    }
+
+    // OpenGL is context based and thread local
+    // Link OpenGL context to SDL, after this you can load OpenGL functions and start rendering
+    // Multi-threading -> Define multiple context and make them current
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext)
+    {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Init all OpenGL function pointers at runtime (not linked at compile time)
+    gladLoadGL();
+
+    // Used for mapping NDC coordinates (-1.0f to 1.0f) to pixel coordinates (e.g. 1920x1080)
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // IMGUI
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForOpenGL(window, glContext);
+    ImGui_ImplOpenGL3_Init();
+
+    // Create & compile vertex and fragment shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Create Program and bind shaders
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Delete shaders since we've created a program already and they are contained there
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Create Triangle Data
+    GLfloat vertices[] =
+    {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+        -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+        0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+        0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f
+    };
+
+    GLuint indices[] =
+    {
+        0, 3, 5,
+        3, 2, 4,
+        5, 4, 1
+    };
+
+    // Create VAO & VBO & EBO
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+
+    // Bind VAO and VBO
+    glBindVertexArray(VAO);
+
+    // Link GL_ARRAY_BUFFER to vertices data
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Link GL_ELEMENT_ARRAY_BUFFER to indices data
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Define Vertex layout and set attribute index
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Unlink VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    bool isRunning = true;
+    while (isRunning)
+    {
+        // INPUT
+        // SDL EVENTS
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                isRunning = false;
+            }
+
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_UP
+                && event.button.button == SDL_BUTTON_RIGHT)
+            {
+            }
+
+            if ((event.type == SDL_EVENT_MOUSE_BUTTON_DOWN
+                && event.button.button == SDL_BUTTON_RIGHT))
+            {
+            }
+
+        }
+
+        // SDL KEYS
+        int numKeys;
+        const bool* keys = SDL_GetKeyboardState(&numKeys);
+        if (keys[SDL_SCANCODE_W])
+        {
+        }
+
+        // UPDATE
+        // [...]
+
+        // RENDER
+        // Clear screen color
+        glClearColor(0.1f, 0.2f, 0.2f, 1.0f);
+
+        // Clear Color Buffer and Depth Buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // RENDER
+        // Use shader program & bind VAO
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+
+        // Specify primitive type and vertex count
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        // Show sample demo from ImGui
+        ImGui::ShowDemoWindow();
+
+        // Render ImGui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Swap window
+        SDL_GL_SwapWindow(window);
+
+        // FRAME CONTROL
+        // [...]
+    }
+
+    // Delete VAO, VBO and shader program
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    // Deinit ImGui
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    // DeInit SDL
+    SDL_GL_DestroyContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
